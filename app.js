af5076e3c84249c56f68f6a4ebefd459bb153cb7
PARME Simon <simon.parme@renault.com>
2025-09-09 15:37:15 +0200

    feat : api rest

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('main branch')
-})
+const hostname = '127.0.0.1'
+const port = process.env.PORT || 3000
+const routes = require("./routes")
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+app.use("/api", routes)
+
+app.listen(port, hostname, () => {
+	console.log(`Serveur démarré sur http://${hostname}:${port}`)
 })
---
+++ controllers/tasks.js
@@ -0,0 +1,53 @@
+const taskService = require('../services/tasks');
+
+function list(req, res) {
+  const tasks = taskService.findAll();
+  res.status(200).json(tasks);
+}
+
+function read(req, res) {
+  const taskId = req.params.id;
+  const task = taskService.find(taskId);
+  if (task)
+    res.status(200).json(task);
+  else
+    res.status(404).json({ message: "Tâche non trouvée" });
+}
+
+function create(req, res) {
+  const datas = req.body;
+  const createdTask = taskService.create(datas);
+  if (createdTask)
+    res.status(201).json({ message: "Tâche créée" });
+  else
+    res.status(400).json({ message: "Erreur lors de l'insertion" });
+}
+
+function update(req, res) {
+  const taskId = req.params.id;
+  const datas = req.body;
+  const updatedTask = taskService.update(taskId, datas);
+  if (updatedTask) {
+    res.status(200).json({ message: "Tâche éditée" });
+  } else {
+    res.status(400).json({ message: "Erreur lors de l'édition" });
+  }
+}
+
+function remove(req, res) {
+  const taskId = req.params.id;
+  const removedTask = taskService.remove(taskId);
+  if (removedTask) {
+    res.status(200).json({ message: "Tâche supprimée" });
+  } else {
+    res.status(400).json({ message: "Erreur lors de la suppression" });
+  }
+}
+
+module.exports = {
+  list,
+  read,
+  create,
+  update,
+  remove
+};
